c6de8c7caec8b75215789e5bddb6e7679bb5e9aa
Roman Kotov <waydeveloper@gmail.com>
2018-06-11 01:00:12 +0300

    Initial Commit

---
+++ RKTableAdapter/RKTableAdapter.h
@@ -0,0 +1,19 @@
+//
+//  RKTableAdapter.h
+//  RKTableAdapter
+//
+//  Created by Roman Kotov on 11/06/2018.
+//  Copyright © 2018 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKTableAdapter.
+FOUNDATION_EXPORT double RKTableAdapterVersionNumber;
+
+//! Project version string for RKTableAdapter.
+FOUNDATION_EXPORT const unsigned char RKTableAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKTableAdapter/PublicHeader.h>
+
+
